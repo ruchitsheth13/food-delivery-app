fdea6102d3e8e25754ccdc15ee62e2ea1b46e385
Ruchit Sheth <ruchitsheth127@gmail.com>
2025-04-14 13:49:58 +0530

    Food items added

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,11 +1,11 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-
+    const [cartItems, setCartItems] = useState()
 
     const contextValue = {
         food_list
