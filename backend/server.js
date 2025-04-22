e002bd727f85efe4199e0d972a2eb38ea48f2989
Ruchit Sheth <ruchitsheth127@gmail.com>
2025-04-22 19:37:34 +0530

    Added food route and API endpoint to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 
@@ -14,6 +15,9 @@ app.use(cors())
 
 // db connection
 connectDB();
+
+// api endpoint
+app.use("/api/food", foodRouter);
 
 app.get("/", (req,res)=>{
     res.send("API Working")
