b21bb12c8ea4adf67376fc626650c1b884a8e152
Ruchit Sheth <ruchitsheth127@gmail.com>
2025-04-22 23:40:17 +0530

    editing navbar and sidebar

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Sidebar.css';
+import { assets } from '../../assets/assets';
 
 const Sidebar = () => {
   return (
-    <div>
-      
+    <div className='sidebar'>
+        
     </div>
   )
 }
